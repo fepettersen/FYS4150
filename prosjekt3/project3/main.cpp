94d205c77003f9c20225cc8fe485ecfe98f97d06
Fredrik <fredrik@fredrik-Aspire-V3-571.(none)>
2012-10-15 11:42:09 +0200

    kskf

---
+++ prosjekt3/project3/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fredrik
+ *
+ * Created on 15. oktober 2012, 09:58
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
