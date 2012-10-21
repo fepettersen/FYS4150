0b65f8e6d043e3d6f06a7502302a347468f67dc7
Fredrik <fredrik@fredrik-Aspire-V3-571.(none)>
2012-10-21 13:49:01 +0200

    a and b

---
+++ prosjekt3/project3/integrate.h
@@ -0,0 +1,158 @@
+/* 
+ * File:   integrate.h
+ * Author: fredrik
+ *
+ * Created on 19. oktober 2012, 11:20
+ */
+#include <cstdlib>
+
+/*Integration program. Solves a 6 dimensional integral by Gauss-Legendre quadrature
+  and by Monte Carlo integration*/
+#include <armadillo>
+#include <cmath>
+#include <iostream>
+#include <time.h>
+using namespace std;
+using namespace arma;
+
+#define EPS 3.0e-14
+#define MAXIT 10
+
+#ifndef INTEGRATE_H
+#define	INTEGRATE_H
+
+void gauleg(double x1, double x2, vec &x, vec &w, int n);
+double f(double x1,double x2,double y1,double y2, double z1, double z2);
+double gammln( double xx);
+double timediff(double time1, double time2);
+double f_sub(double r1,double r2,double theta1,double theta2,double phi1,double phi2);
+void gauss_laguerre(vec &x, vec &w, int n, double alpha);
+#endif	/* INTEGRATE_H */
+
+
+
+void gauleg(double x1, double x2, vec &x, vec &w, int n)
+{
+   int         m,j,i,s;
+   double      z1,z,xm,xl,pp,p3,p2,p1;
+   double      const  pi = 3.14159265359; 
+   s=n-1;
+   m  = (n + 1)/2;
+   xm = 0.5 * (x2 + x1);
+   xl = 0.5 * (x2 - x1);
+
+   for(i = 1; i <= m; i++) {                             // loops over desired roots
+      z = cos(pi * (i - 0.25)/(n + 0.5));
+
+           /*
+	   ** Starting with the above approximation to the i'th root
+           ** we enter the main loop of refinement by Newtons method.
+           */
+
+      do {
+         p1 =1.0;
+	 p2 =0.0;
+
+   	   /*
+	   ** loop up recurrence relation to get the
+           ** Legendre polynomial evaluated at x
+           */
+
+	 for(j = 1; j <= n; j++) {
+	    p3 = p2;
+	    p2 = p1;
+	    p1 = ((2.0 * j - 1.0) * z * p2 - (j - 1.0) * p3)/j;
+	 }
+
+	   /*
+	   ** p1 is now the desired Legrendre polynomial. Next compute
+           ** ppp its derivative by standard relation involving also p2,
+           ** polynomial of one lower order.
+           */
+ 
+	 pp = n * (z * p1 - p2)/(z * z - 1.0);
+	 z1 = z;
+	 z  = z1 - p1/pp;                   // Newton's method
+      } while(fabs(z - z1) > 1e-10);
+
+          /* 
+	  ** Scale the root to the desired interval and put in its symmetric
+          ** counterpart. Compute the weight and its symmetric counterpart
+          */
+      x(i-1) = xm - xl * z;
+      x(s) = xm + xl * z;
+      w(i-1) = 2.0 * xl/((1.0 - z * z) * pp * pp);
+      w(s) = w(i-1);
+      s--;
+      
+   }
+} // End_ function gauleg()
+double f(double x1,double x2,double y1,double y2, double z1, double z2){
+    double denom = (fabs(sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))));
+    double fexp = -4*(sqrt(x1*x1+y1*y1+z1*z1)+sqrt(x2*x2+y2*y2+z2*z2));
+    double func =  exp(fexp)/denom;
+    return (denom>1e-12)?func:0;
+}
+double timediff(double time1, double time2){
+    // This function returns the elapsed time in milliseconds
+    return ((time2 - time1)*1000)/CLOCKS_PER_SEC;
+}
+double f_sub(double r1,double r2,double theta1,double theta2,double phi1,double phi2){
+    double denom = (r1*r1 + r2*r2 -\
+r1*r2*(cos(theta1+theta2)*(1-cos(phi1-phi2))+cos(theta1-theta2)*(1+cos(phi1-phi2))));
+    //double pow = -1*(r1+r2);
+    double kapow = sin(theta1)*sin(theta2)/sqrt(denom);
+    return (denom>1e-12)?kapow:0;
+}
+void gauss_laguerre(vec &x, vec &w, int n, double alpha)
+{
+	int i,its,j;
+	double ai;
+	double p1,p2,p3,pp,z,z1;
+
+	for (i=1;i<=n;i++) {
+		if (i == 1) {
+			z=(1.0+alpha)*(3.0+0.92*alpha)/(1.0+2.4*n+1.8*alpha);
+		} else if (i == 2) {
+			z += (15.0+6.25*alpha)/(1.0+0.9*alpha+2.5*n);
+		} else {
+			ai=i-2;
+			z += ((1.0+2.55*ai)/(1.9*ai)+1.26*ai*alpha/
+				(1.0+3.5*ai))*(z-x(i-2))/(1.0+0.3*alpha);
+                        
+		}
+		for (its=1;its<=MAXIT;its++) {
+			p1=1.0;
+			p2=0.0;
+			for (j=1;j<=n;j++) {
+				p3=p2;
+				p2=p1;
+				p1=((2*j-1+alpha-z)*p2-(j-1+alpha)*p3)/j;
+			}
+			pp=(n*p1-(n+alpha)*p2)/z;
+			z1=z;
+			z=z1-p1/pp;
+			if (fabs(z-z1) <= EPS) break;
+		}
+		if (its > MAXIT) cout << "too many iterations in gaulag" << endl;
+		x(i)=z;
+		w(i) = -exp(gammln(alpha+n)-gammln((double)n))/(pp*n*p2);
+	}
+}
+// end function gaulag
+
+double gammln( double xx)
+{
+	double x,y,tmp,ser;
+	static double cof[6]={76.18009172947146,-86.50532032941677,
+		24.01409824083091,-1.231739572450155,
+		0.1208650973866179e-2,-0.5395239384953e-5};
+	int j;
+
+	y=x=xx;
+	tmp=x+5.5;
+	tmp -= (x+0.5)*log(tmp);
+	ser=1.000000000190015;
+	for (j=0;j<=5;j++) ser += cof[j]/++y;
+	return -tmp+log(2.5066282746310005*ser/x);
+}
--- prosjekt3/project3/main.cpp
+++ prosjekt3/project3/main.cpp
@@ -5,80 +5,12 @@
  * Created on 15. oktober 2012, 09:58
  */
 
-#include <cstdlib>
+#include "integrate.h"
 
-/*Integration program. Solves a 6 dimensional integral by Gauss-Legendre quadrature
-  and by Monte Carlo integration*/
-#include <armadillo>
-#include <cmath>
-#include <iostream>
-using namespace std;
-using namespace arma;
-
-void gauleg(double x1, double x2, vec &x, vec &w, int n)
-{
-   int         m,j,i,s;
-   double      z1,z,xm,xl,pp,p3,p2,p1;
-   double      const  pi = 3.14159265359; 
-   s=n-1;
-   m  = (n + 1)/2;
-   xm = 0.5 * (x2 + x1);
-   xl = 0.5 * (x2 - x1);
-
-   for(i = 1; i <= m; i++) {                             // loops over desired roots
-      z = cos(pi * (i - 0.25)/(n + 0.5));
-
-           /*
-	   ** Starting with the above approximation to the i'th root
-           ** we enter the main loop of refinement by Newtons method.
-           */
-
-      do {
-         p1 =1.0;
-	 p2 =0.0;
-
-   	   /*
-	   ** loop up recurrence relation to get the
-           ** Legendre polynomial evaluated at x
-           */
-
-	 for(j = 1; j <= n; j++) {
-	    p3 = p2;
-	    p2 = p1;
-	    p1 = ((2.0 * j - 1.0) * z * p2 - (j - 1.0) * p3)/j;
-	 }
-
-	   /*
-	   ** p1 is now the desired Legrendre polynomial. Next compute
-           ** ppp its derivative by standard relation involving also p2,
-           ** polynomial of one lower order.
-           */
- 
-	 pp = n * (z * p1 - p2)/(z * z - 1.0);
-	 z1 = z;
-	 z  = z1 - p1/pp;                   // Newton's method
-      } while(fabs(z - z1) > 1e-10);
-
-          /* 
-	  ** Scale the root to the desired interval and put in its symmetric
-          ** counterpart. Compute the weight and its symmetric counterpart
-          */
-      x(i-1) = xm - xl * z;
-      x(s) = xm + xl * z;
-      w(i-1) = 2.0 * xl/((1.0 - z * z) * pp * pp);
-      w(s) = w(i-1);
-      s--;
-      
-   }
-} // End_ function gauleg()
-double f(double x1,double x2,double y1,double y2, double z1, double z2){
-    double denom = (fabs(sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))));
-    double fexp = -4*(sqrt(x1*x1+y1*y1+z1*z1)+sqrt(x2*x2+y2*y2+z2*z2));
-    double func =  exp(fexp)/denom;
-    return (denom>1e-12)?func:0;
-}
+#define PI 3.14159
 int main(int argc, char** argv){
-    int n = 20;//atoi(argv[1]);
+    int n =50;//atoi(argv[1]);
+#if 1
     vec x1(n),x2(n),y1(n),y2(n),z1(n),z2(n),w1(n),w2(n),w3(n),w4(n),w5(n),w6(n);
     x1.zeros();x2.zeros();y1.zeros();y2.zeros();z1.zeros();z2.zeros();
     w1.zeros();w2.zeros();w3.zeros();w4.zeros();w5.zeros();w6.zeros();
@@ -91,26 +23,91 @@ int main(int argc, char** argv){
     gauleg(-lambda,lambda,z1,w5,n);
     gauleg(-lambda,lambda,z2,w6,n);
     //w2.print("w2:");
+
     double sum = 0.0;
     u_long counter=0;
-    
-    #pragma omp parallel for
-    for (int i=0;i<n;i++){
+    //int i,j,k,l,m,p;
+    double start = clock();
+#pragma omp parallel shared(x1,x2,y1,y2,z2,z1,n,w1,w2,w3,w4,w5,w6,sum,counter)
+    {
+        u_long l_counter = 0;
+        double l_sum = 0;
+#pragma omp for 
+
+    for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             for(int k=0;k<n;k++){
                 for(int l=0;l<n;l++){
                     for(int m=0;m<n;m++){
                         for(int p=0;p<n;p++){
-                            sum+=w1(i)*w2(j)*w3(k)*w4(l)*w5(m)*w6(p)*f(x1(i),x2(j),y1(k),y2(l),z1(m),z2(p));
+                            l_sum+=w1(i)*w2(j)*w3(k)*w4(l)*w5(m)*w6(p)*f(x1(i),x2(j),y1(k),y2(l),z1(m),z2(p));
                             //cout<<"f(x,y,z) = "<<f(x1(i),x2(j),y1(k),y2(l),z1(m),z2(p))<<endl;
-                            counter++;
+                            l_counter++;
                         }
                     }
                 }
             }
         }
     }
-    cout<<"sum = "<<sum<<" after "<<counter<<" steps"<<endl;
+    
+    #pragma omp critical
+        {sum += l_sum; counter += l_counter;}
+    
+    }
+    double stop = clock();
+    double diff = timediff(start,stop)/4;
+    u_long n6 = n*n*n*n*n*n;
+    cout<<"sum = "<<sum<<" in "<<diff<<" ms, steps: "<<counter<<" out of "<<n6<<endl;
+    cout<<"correct sum  = "<<((5*PI*PI)/(16*16))<<endl;
+ #else 
+    vec r(n+1),theta(n),phi(n),w1(n+1),w2(n),w3(n);
+    r.zeros();theta.zeros();phi.zeros();
+    w1.zeros();w2.zeros();w3.zeros();
+            
+    //double lambda = 3.0;
+    gauleg(0,PI,theta,w2,n);
+    gauleg(0,2*PI,phi,w3,n);
+    gauss_laguerre(r, w1,n,2.0);
+    //w2.print("w2:");
+
+    double sum = 0.0;
+    u_long counter=0;
+    
+    double start = clock();
+    w1.print("w1:");
+   
+#pragma omp parallel shared(r,theta,phi,n,w1,w2,w3,sum,counter)
+    {
+        u_long l_counter = 0;
+        double l_sum = 0;
+        //int i,j,k,l,m,p;
+#pragma omp for 
+    for(int i=0;i<n;i++){
+        cout<<i<<" out of "<<n<<" iterations"<<endl;
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                for(int l=0;l<n;l++){
+                    for(int m=0;m<n;m++){
+                        for(int p=0;p<n;p++){
+                            l_sum+=w1(i+1)*w1(j+1)*w2(k)*w2(l)*w3(m)*w3(p)*\
+                                    f_sub(r(i+1),r(j+1),theta(k),theta(l),phi(m),phi(p));
+                            l_counter++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+#pragma omp critical
+        {sum += l_sum; counter += l_counter;}
+    }
+    sum *=1.0/1024;
+    double stop = clock();
+    double diff = timediff(start,stop);
+    u_long n6 = n*n*n*n*n*n;
+    cout<<"sum = "<<sum<<" in "<<diff<<" ms, steps: "<<counter<<" out of "<<n6<<endl;
+    cout<<"correct sum  = "<<((5*PI*PI)/(16*16))<<endl;
+#endif
     return 0;
 }        
 
