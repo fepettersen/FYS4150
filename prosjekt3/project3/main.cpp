717878b399811a2a28ad471e2e399b1f8a1a572b
Fredrik <fredrik@fredrik-Aspire-V3-571.(none)>
2012-10-28 15:15:39 +0100

    updates

--- prosjekt3/project3/main.cpp
+++ prosjekt3/project3/main.cpp
@@ -9,8 +9,9 @@
 
 #define PI 3.14159
 int main(int argc, char** argv){
-    int n =atoi(argv[1]);//atoi(argv[1]);
-#if 0
+    int n =atoi(argv[1]);
+    int k = atoi(argv[2]);      /*Read number of cores to use from commandline*/
+#if 1
     vec x1(n),x2(n),y1(n),y2(n),z1(n),z2(n),w1(n),w2(n),w3(n),w4(n),w5(n),w6(n);
     x1.zeros();x2.zeros();y1.zeros();y2.zeros();z1.zeros();z2.zeros();
     w1.zeros();w2.zeros();w3.zeros();w4.zeros();w5.zeros();w6.zeros();
@@ -22,13 +23,12 @@ int main(int argc, char** argv){
     gauleg(-lambda,lambda,y2,w4,n);
     gauleg(-lambda,lambda,z1,w5,n);
     gauleg(-lambda,lambda,z2,w6,n);
-    //w2.print("w2:");
-
+    int num_cores = 0;
     double sum = 0.0;
     u_long counter=0;
-    //int i,j,k,l,m,p;
     double start = clock();
-#pragma omp parallel shared(x1,x2,y1,y2,z2,z1,n,w1,w2,w3,w4,w5,w6,sum,counter)
+#pragma omp parallel shared(x1,x2,y1,y2,z2,z1,n,w1,w2,w3,w4,w5,w6,sum,counter) \
+num_threads(k)
     {
         u_long l_counter = 0;
         double l_sum = 0;
@@ -40,8 +40,8 @@ int main(int argc, char** argv){
                 for(int l=0;l<n;l++){
                     for(int m=0;m<n;m++){
                         for(int p=0;p<n;p++){
-                            l_sum+=w1(i)*w2(j)*w3(k)*w4(l)*w5(m)*w6(p)*f(x1(i),x2(j),y1(k),y2(l),z1(m),z2(p));
-                            //cout<<"f(x,y,z) = "<<f(x1(i),x2(j),y1(k),y2(l),z1(m),z2(p))<<endl;
+                            l_sum+=w1(i)*w2(j)*w3(k)*w4(l)*w5(m)*w6(p)*\
+                                    f(x1(i),x2(j),y1(k),y2(l),z1(m),z2(p));
                             l_counter++;
                         }
                     }
@@ -51,30 +51,32 @@ int main(int argc, char** argv){
     }
     
     #pragma omp critical
-        {sum += l_sum; counter += l_counter;}
+        {
+        sum += l_sum; 
+        counter += l_counter;
+        num_cores = omp_get_num_threads();
+        }
     
     }
     double stop = clock();
-    double diff = timediff(start,stop)/4;
+    double diff = timediff(start,stop)/((double) k);
     u_long n6 = n*n*n*n*n*n;
     cout<<"sum = "<<sum<<" in "<<diff<<" ms, steps: "<<counter<<" out of "<<n6<<endl;
-    cout<<"correct sum  = "<<((5*PI*PI)/(16*16))<<endl;
+    cout<<"correct sum  = "<<((5*PI*PI)/(16*16))<<" number of cores used: "<<num_cores<<endl;
  #else 
     vec r(n+1),theta(n),phi(n),w1(n+1),w2(n),w3(n);
     r.zeros();theta.zeros();phi.zeros();
     w1.zeros();w2.zeros();w3.zeros();
-            
-    //double lambda = 3.0;
+
     gauleg(0,PI,theta,w2,n);
     gauleg(0,2*PI,phi,w3,n);
     gauss_laguerre(r, w1,n,2.0);
-    //w2.print("w2:");
 
     double sum = 0.0;
     u_long counter=0;
     
     double start = clock();
-    w1.print("w1:");
+    
    
 #pragma omp parallel shared(r,theta,phi,n,w1,w2,w3,sum,counter)
     {
@@ -83,7 +85,7 @@ int main(int argc, char** argv){
         //int i,j,k,l,m,p;
 #pragma omp for 
     for(int i=0;i<n;i++){
-        cout<<i<<" out of "<<n<<" iterations"<<endl;
+        //cout<<i<<" out of "<<n<<" iterations"<<endl;
         for(int j=0;j<n;j++){
             for(int k=0;k<n;k++){
                 for(int l=0;l<n;l++){
@@ -99,7 +101,10 @@ int main(int argc, char** argv){
         }
     }
 #pragma omp critical
-        {sum += l_sum; counter += l_counter;}
+        {
+         sum += l_sum; 
+         counter += l_counter;
+        }
     }
     sum *=1.0/1024;
     double stop = clock();
@@ -108,32 +113,7 @@ int main(int argc, char** argv){
     cout<<"sum = "<<sum<<" in "<<diff<<" ms, steps: "<<counter<<" out of "<<n6<<endl;
     cout<<"correct sum  = "<<((5*PI*PI)/(16*16))<<endl;
 #endif
-    //------------Monte Carlo--------------
-    
-    long idum = -1;
-    int N = 1e4;
-    double crude_mc , x1,x2,y1,y2,z1,z2 , sum_sigma , fx,variance;
-    crude_mc=sum_sigma = 0;
-    for(int i=0;i<N;i++){
-        x1 = -lambda +2*lambda*ran0(&idum);
-        x2 = -lambda +2*lambda*ran0(&idum);
-        y1 = -lambda +2*lambda*ran0(&idum);
-        y2 = -lambda +2*lambda*ran0(&idum);
-        z1 = -lambda +2*lambda*ran0(&idum);
-        z2 = -lambda +2*lambda*ran0(&idum);
-        fx = f(x1,x2,y1,y2,z1,z2);
-        crude_mc += fx;
-        sum_sigma += fx*fx;
-    }
-    /*
-    Random * rnd = new Random(-1);
-    rnd->nextDouble();
-    */
-    crude_mc /= (double) N;
-    sum_sigma /= (double) N;
-    variance = sum_sigma - crude_mc*crude_mc;
-    cout<<"Monte Carlo simulation with N = "<<N<<" gives "<<crude_mc<<endl;
-    cout<<"The variance is "<<variance<<" and standard deviation "<<sqrt(variance)<<endl;
+
     return 0;
 }        
 
--- prosjekt3/project3/montecarlo.cpp
+++ prosjekt3/project3/montecarlo.cpp
@@ -60,30 +60,39 @@ int main(int argc, char** argv) {
     double start = clock();
 #pragma omp parallel 
     {
+        /*Set up private variables for the paralell execution of this loop*/
         double l_sum = 0;
+        double l_sum2 = 0;
         double r1,r2,theta1,theta2,phi1,phi2;
         double fx = 0;
         double sum_sigma = 0;
+        double sum_sigma2 = 0;
         long idum1,idum2,idum3,idum4,idum5,idum6;
         idum1 = time(0)-123; idum2 = time(0)-383;idum3 = time(0)-73;idum4 = time(0)-239; 
         idum5 = time(0)-830;idum6 = time(0)-955;
 #pragma omp for
     
     for(int i=0;i<N;i++){
-        r1 = -0.25*log(1-ran0(&idum1));
-        r2 = -0.25*log(1-ran0(&idum2));
+        r1 = -log(1-ran0(&idum1));
+        r2 = -log(1-ran0(&idum2));
         theta1 = PI*ran0(&idum3);
         theta2 = PI*ran0(&idum4);
         phi1 = 2*PI*ran0(&idum5);
         phi2 = 2*PI*ran0(&idum6);
-        fx = 4*r1*r1*r2*r2*f_sub(r1,r2,theta1,theta2,phi1,phi2);  ///exp(-(r1+r2))
-        l_sum += fx;
-        sum_sigma += fx*fx;
+        fx = (1./1024)*r1*r1*r2*r2*f_sub(r1,r2,theta1,theta2,phi1,phi2);
+        if (l_sum > 1e4){
+            l_sum2 +=fx;
+            sum_sigma2 +=fx*fx;
+        }
+        else{
+                l_sum += fx;
+                sum_sigma += fx*fx;
+        }
     }
 #pragma omp critical
         {
-        crude_mc +=l_sum; 
-        g_sigma += sum_sigma; 
+        crude_mc +=l_sum+l_sum2; 
+        g_sigma += sum_sigma+sum_sigma2; 
         num_cores = omp_get_num_threads();
         }
     }
@@ -92,10 +101,10 @@ int main(int argc, char** argv) {
     crude_mc /= ((double) N);
     g_sigma /= ((double) N);
     variance = g_sigma - crude_mc*crude_mc;
-    crude_mc *= (1.0/16.0)*pow(PI,4);
+    crude_mc *= 4*pow(PI,4);
     cout<<"Monte Carlo simulation with (importance sampling) N = "<<N<<" gives "<<crude_mc<<endl;
     cout<<"correct result: "<<5*PI*PI/(16*16)<<endl;
-    cout<<"The variance is "<<variance<<" and standard deviation "<<sqrt(variance)<<endl;
+    cout<<"The variance is "<<variance<<" and standard deviation "<<pow(4,PI)*sqrt(variance/((double) N))<<endl;
     cout<<"This took "<<diff<<" ms on "<<num_cores<<" cores"<<endl;
 #endif
     return 0;
