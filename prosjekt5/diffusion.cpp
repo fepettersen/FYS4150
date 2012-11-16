cea8d15d5aadc7736ef99a544b5be36d0e64bb2a
Fredrik <fredrik@fredrik-Aspire-V3-571.(none)>
2012-11-16 16:50:59 +0100

    started with project 5

---
+++ prosjekt5/diffusion.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: fredrik
+ *
+ * Created on 15. oktober 2012, 09:58
+ */
+
+#include "diffusion.h"
+
+#define PI 3.14159
+int main(int argc, char** argv){
+    
+    return 0;
+}        
+
+
